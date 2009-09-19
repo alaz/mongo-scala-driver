7cf52b158624e793a168af97e017fe1a321abf29
Alexander Azarov <azarov@aha.ru>
2009-09-19 17:51:39 +0400

    store/retrieve shape tests

--- src/test/scala/com/osinka/mongodb/shape/shapeSpec.scala
+++ src/test/scala/com/osinka/mongodb/shape/shapeSpec.scala
@@ -11,26 +11,42 @@ class shapeTest extends JUnit4(shapeSpec) with Console
 object shapeTestRunner extends ConsoleRunner(shapeSpec)
 
 object shapeSpec extends Specification("Scala way Mongo shapes") {
+    val CollName = "test"
+    val Const = "John Doe"
+
     val mongo: Mongo = new Mongo(DbAddress)
 
     doAfter { mongo.dropDatabase }
 
     "Shaped collection" should {
-        val dbColl = mongo.getCollection("test")
+        val dbColl = mongo.getCollection(CollName)
 
-        doBefore {
-            dbColl.drop; mongo.requestStart
-            dbColl save Map("name" -> "John Doe")
-        }
+        doBefore { dbColl.drop; mongo.requestStart }
         doAfter  { mongo.requestDone; dbColl.drop }
 
-        "retrieve ordinary class objects" in {
+        "retrieve objects / ord" in {
+            dbColl save Map("name" -> Const)
             val coll = dbColl.of(OrdUser)
             coll must haveSuperClass[ShapedCollection[OrdUser]]
+            coll.firstOption must beSome[OrdUser].which{x => x.name == Const && x.mongoOID != null && x.mongoNS == CollName}
         }
-        "retrieve case class objects" in {
+        "retrieve objects / case" in {
+            dbColl save Map("name" -> Const)
             val coll = dbColl.of(CaseUser)
             coll must haveSuperClass[ShapedCollection[CaseUser]]
+            coll.firstOption must beSome[CaseUser].which{x => x.name == Const && x.mongoOID != null && x.mongoNS == CollName}
+        }
+        "store objects / case" in {
+            val coll = dbColl.of(CaseUser)
+            coll save CaseUser(Const)
+            coll.firstOption must beSome[CaseUser].which{x => x.name == Const && x.mongoOID != null && x.mongoNS == CollName}
+        }
+        "store objects / ord" in {
+            val coll = dbColl.of(OrdUser)
+            val u = new OrdUser
+            u.name = Const
+            coll save u
+            coll.firstOption must beSome[OrdUser].which{x => x.name == Const && x.mongoOID != null && x.mongoNS == CollName}
         }
     }
 }
