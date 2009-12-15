80e145f95d2ed6c29872128be99759aeef2f904b
Alexander Azarov <azarov@aha.ru>
2009-12-15 18:27:11 +0300

    Option[A] field model

--- src/test/scala/com/osinka/mongodb/shape/OptModel.scala
+++ src/test/scala/com/osinka/mongodb/shape/OptModel.scala
@@ -2,7 +2,6 @@ package com.osinka.mongodb.shape
 
 import com.mongodb._
 
-/*
 class OptModel(val id: Int, val description: Option[String]) {
     var comment: String = OptModel.DefaultComment
 }
@@ -11,16 +10,17 @@ object OptModel extends ObjectShape[OptModel] {
     val DefaultComment = "default"
 
     lazy val id = Scalar("id", _.id)
-    lazy val description = Scalar("description", _.description)
+    object description extends Scalar[Option[String]]("description", _.description) with Functional[Option[String]] {
+        override val serializer = opt[String] + defl[String]
+    }
     object comment extends Scalar[String]("comment", _.comment) with Updatable[String] with Optional[String] {
         override def update(obj: OptModel, v: String) { obj.comment = v }
     }
 
-    override def * = id :: description :: comment :: Nil
+    override def * = List(id, description, comment)
 
     override def factory(dbo: DBObject) =
         for {id(i) <- Some(dbo)
              description(descr) <- Some(dbo)}
         yield new OptModel(i, descr)
 }
-*/
