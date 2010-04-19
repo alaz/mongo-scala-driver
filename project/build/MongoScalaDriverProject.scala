b10bb10167e015a97684ee126ea263a3b192adf3
Alexander Azarov <azarov@osinka.ru>
2010-04-19 23:12:06 +0400

    benchmark action in sbt

--- project/build/MongoScalaDriverProject.scala
+++ project/build/MongoScalaDriverProject.scala
@@ -20,6 +20,17 @@ class MongoScalaDriverProject(info: ProjectInfo) extends DefaultProject(info) {
     // Packaging
     override def packageSrcJar= defaultJarPath("-src.jar")
 
+    // Benchmark action
+    val benchmarkConf = config("benchmark")
+    def benchmarkClasspath = fullClasspath(benchmarkConf) +++ testClasspath
+
+    def benchmarkTask(args: List[String]) =
+        runTask(Some("com.osinka.mongodb.benchmark.overhead"), benchmarkClasspath, args) .
+        dependsOn(compile, testCompile) .
+        describedAs("Bechmark Scala driver")
+
+    lazy val benchmark = benchmarkTask("50000" :: "4" :: Nil)
+
     override def compileOptions = super.compileOptions ++ extraCompileOptions.map(x => CompileOption(x))
 
     override def pomExtra = {
